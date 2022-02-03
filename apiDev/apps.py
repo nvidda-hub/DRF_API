e1bc2cb5a4ab4d16c9942990fa4aac387f095b62
nvidda-hub <nkvidda@gmail.com>
2022-02-03 12:06:45 +0530

    working api

---
+++ apiDev/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApidevConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apiDev'
---
+++ apiDev/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from apiDev import views
+from rest_framework.routers import DefaultRouter
+
+
+app_name = 'apiDev'
+
+router = DefaultRouter()
+
+router.register('article', views.ArticleViewSet, basename="article")
+
+urlpatterns = [
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
+]
